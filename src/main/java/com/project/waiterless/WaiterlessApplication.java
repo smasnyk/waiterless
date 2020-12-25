17508bf23e944a46784abcdca0a5541d20aafa67
smasnyk <solomiia.masnyk@gmail.com>
2020-12-25 22:06:50 +0200

    Add project files

---
+++ src/main/java/com/project/waiterless/WaiterlessApplication.java
@@ -0,0 +1,13 @@
+package com.project.waiterless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaiterlessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaiterlessApplication.class, args);
+    }
+
+}
